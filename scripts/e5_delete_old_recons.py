170c6a625ea96625c64290de6fb403710b22955c
Quintin1995 <18417076+Quintin1995@users.noreply.github.com>
2025-06-12 15:43:57 +0200

    The DB is now full of Spearman Correlation for each patient, acceleration, region, uq_method and slice.

--- scripts/e5_delete_old_recons.py
+++ scripts/e5_delete_old_recons.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 if __name__ == "__main__":
-    root_dir = Path('/scratch/hb-pca-rad/projects/04_uncertainty_quantification/gaussian/recons_3x')
+    root_dir = Path('/scratch/hb-pca-rad/projects/04_uncertainty_quantification/gaussian/recons_6x')
 
     for subfolder in root_dir.iterdir():
         if subfolder.is_dir():
-            for file in subfolder.glob('vsharp_r6_recon_dcml_gaus*.nii.gz'):
+            for file in subfolder.glob('uq_map_R6_gm25.nii.gz'):
                 try:
                     file.unlink()
                     print(f"Deleted: {file.resolve()}")
